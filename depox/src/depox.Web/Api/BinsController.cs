e2b913dc7739c876ec727da1ceeaffb0d19ff7b7
Ibraim Sapountzi <ibraimsap@gmail.com>
2020-10-18 23:31:17 +0300

    api; add bin management with basic crud actions; add in comments getBinItems by injecting dbcontext; needs refactor efrepositor to support specifications;

---
+++ depox/src/depox.Web/Api/BinsController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using depox.Core.Entities;
+using depox.SharedKernel.Interfaces;
+using depox.Web.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using depox.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace depox.Web.Api
+{
+    public class BinsController : BaseApiController
+    {
+        private readonly IRepository<Bin> _repository;
+
+        //public readonly AppDbContext _dbContext;
+        public BinsController(IRepository<Bin> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Bins
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var bins = (await _repository.ListAsync())
+                            .Select(BinDto.FromBin);
+            return Ok(bins);
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var item = BinDto.FromBin(await _repository.GetByIdAsync(id));
+            return Ok(item);
+        }
+
+        // POST: api/ToDoItems
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] BinDto bin)
+        {
+            var newBin = new Bin()
+            {
+                Code = bin.Code,
+                Name = bin.Name,
+                Description = bin.Description,
+            };
+            await _repository.AddAsync(newBin);
+            return Ok(BinDto.FromBin(newBin));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] BinDto bin)
+        {
+            var updatedBin = new Bin()
+            {
+                Id = bin.Id,
+                Code = bin.Code,
+                Name = bin.Name,
+                Description = bin.Description
+            };
+            await _repository.UpdateAsync(updatedBin);
+
+            return Ok(BinDto.FromBin(updatedBin));
+        }
+
+        //[HttpGet("{id:int}/items")]
+        //public async Task<IActionResult> GetBinItems(int id)
+        //{
+        //    var bins = _dbContext.Bins.Where(bin => bin.Id == id).Include(b => b.Items).Select(b => b.Items).ToList();
+
+        //    return Ok(bins);
+        //}
+
+    }
+}
---
+++ depox/src/depox.Web/ApiModels/BinDto.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using depox.Core.Entities;
+
+namespace depox.Web.ApiModels
+{
+    public class BinDto
+    {
+        public  int Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public static BinDto FromBin(Bin bin)
+        {
+            return new BinDto()
+            {
+                Id = bin.Id,
+                Code = bin.Code,
+                Name = bin.Name,
+                Description = bin.Description
+            };
+        }
+
+    }
+
+    public class BinItemsDto
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public static BinItemsDto FromBin(Bin bin)
+        {
+            return new BinItemsDto()
+            {
+                Id = bin.Id,
+                Code = bin.Code,
+                Items = bin.Items.ToList()
+            };
+        }
+    }
+}
